3cbedaf7513f209ab260e73bb2415b74be54a782
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-06 14:50:19 +0200

    emacs-29 introduces pixel-scroll-precision-mode

--- custom/cust_scroll.el
+++ custom/cust_scroll.el
@@ -15,5 +15,13 @@
   (add-hook 'after-make-frame-functions 'my/disable-scroll-bars)
   )
 
+;; Enable smooth scrolling strating from emacs-29
+(when (and (display-graphic-p)
+	   (fboundp 'pixel-scroll-precision-mode)
+	   (not (version< emacs-version "29.0")))
+  (pixel-scroll-precision-mode 1)
+  (setq pixel-scroll-precision-large-scroll-height 40.0)
+  (setq pixel-scroll-precision-interpolation-factor 30))
+
 (provide 'cust_scroll)
 ;;; cust_scroll ends here
