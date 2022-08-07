94eea1457170ada9397c1a229abdf194fbd2a120
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-07 10:33:13 +0200

    improve avy configuration

--- custom/my_avy.el
+++ custom/my_avy.el
@@ -4,5 +4,16 @@
 
 (use-package avy
   :ensure t
-  :config
-  (avy-setup-default))
+  :init (avy-setup-default)
+  :config (progn
+			;; Makes it easier to see the candidates
+			(setq avy-background t)
+			;; Shows both the candidates over the text
+			(setq avy-styles-alist '((avy-goto-char-2 . at-full))))
+  :bind (;; Search by first char of a word
+		 ("M-s" . avy-goto-word-1)
+		 ;; Search by 2 chars
+		 ("M-s s" . avy-goto-char-2)
+		 ("M-g M-g" . avy-goto-line))
+  :config (setq avy-all-windows nil)
+  )
