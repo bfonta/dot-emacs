382af97c6a9137d10f852be236269b5dd34c55d6
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-10-31 11:33:29 +0100

    :bugfix: modeline whichfunc

--- custom/my_whichfunc.el
+++ custom/my_whichfunc.el
@@ -5,11 +5,7 @@
 (use-package which-func
   :config
   (setq which-func-modes '(emacs-lisp-mode c++-mode python-mode))
-  ;; replace nothing found symbol "???"
-  (setq which-func-unknown "-- ∅ --")
-  (setq mode-line-format (delete (assoc 'which-func-mode
-                                      mode-line-format) mode-line-format)
-      which-func-header-line-format '(which-func-mode ("" which-func-format)))
+  (setq which-func-unknown "----") ;; replace nothing found symbol "???"
   )
 
 (provide 'my_whichfunc)
