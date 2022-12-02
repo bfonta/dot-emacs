3ff3418337c511d57dda300bcb82891ed7817914
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-12-02 18:36:32 +0100

    :fire: remove spurious mode

--- custom/my_cpp.el
+++ custom/my_cpp.el
@@ -6,9 +6,7 @@
   :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (XXX-mode . lsp)
-         ;; if you want which-key integration
+  :hook (;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp
   :config
