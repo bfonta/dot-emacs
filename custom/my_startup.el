fef7424d6715a4c23100bf7bee93c0785d9b0693
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-12-14 23:00:34 +0000

    :bug: :poop: quick fix font size

--- custom/my_startup.el
+++ custom/my_startup.el
@@ -7,12 +7,13 @@
 ;; Font size adjustment
 (defun my/adjust-font-size-start-up ()
   "Inspired by https://emacs.stackexchange.com/a/44930/17066"
-  (let ((width-px (nth 3 (nth 1 (frame-monitor-attributes)))))
-	(when (eq width-px 1024) ;; Home
-	  (set-face-attribute 'default t :height 120))
-	(when (eq width-px 2560) ;; TP
-	  (set-face-attribute 'default t :height 220))
-	)
+  ;; (let ((width-px (nth 3 (nth 1 (frame-monitor-attributes)))))
+  ;; 	(when (eq width-px 1024) ;; Home
+  ;; 	  (set-face-attribute 'default nil :height 110))
+  ;; 	(when (eq width-px 2560) ;; TP
+  ;; 	  (set-face-attribute 'default nil :height 105))
+  ;; 	)
+  (set-face-attribute 'default nil :height 105)
   )
 (add-hook 'emacs-startup-hook #'my/adjust-font-size-start-up)
 
