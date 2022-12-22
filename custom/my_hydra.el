800f73a5345cf79c076a4d729162da28ad0a4788
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-12-22 16:46:38 +0000

    update keybinds

--- custom/my_hydra.el
+++ custom/my_hydra.el
@@ -7,11 +7,13 @@
 
 (defhydra hydra-dired (:exit t)
   "dired"
-  ("f" (dired "~/llr_fpgas/bye_splits") "fpgas")
-  ("d" (dired "~/llr_fpgas/bye_splits/plot/display") "display")
   ("t" (dired "~/llr_triggers") "triggers")
   ("e" (dired "~/dot-emacs/custom/") "emacs")
-  ("o" (dired "~/org/") "org"))
+  ("o" (dired "~/org/") "org")
+  ("d" (dired "~/Documents/") "Documents")
+  ("1" (dired "~/llr_fpgas/bye_splits") "fpgas")
+  ("2" (dired "~/llr_fpgas/bye_splits/plot/display") "display"))
+
 (global-set-key (kbd "C-x d") 'hydra-dired/body)
 
 (provide 'my_hydra)
