578548143c446be118e12dd87f465aa4458108fc
bfontana <20703947+b-fontana@users.noreply.github.com>
2023-01-12 15:57:13 +0100

    add shortcuts

--- custom/my_hydra.el
+++ custom/my_hydra.el
@@ -13,7 +13,10 @@
   ("d" (dired "~/Documents/") "Documents")
   ("1" (dired "~/llr_fpgas/bye_splits") "fpgas")
   ("2" (dired "~/llr_fpgas/bye_splits/plot/display") "display")
-  ("3" (dired "~/llr_hgctpg/L1Trigger/L1THGCal/test/HGCalTriggerGeomTesterV9Imp3.cc") "hgc-tpg"))
+  ("3" (dired "~/llr_hgctpg/L1Trigger/L1THGCal/test/HGCalTriggerGeomTesterV9Imp3.cc") "hgc-tpg")
+  ("4" (dired "~/klub/") "klub")
+  ("5" (dired "~/big_ntuples/") "big_ntuples")
+  )
 
 (global-set-key (kbd "C-x d") 'hydra-dired/body)
 
