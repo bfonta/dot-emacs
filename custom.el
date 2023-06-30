221e5d287d88ce88807718e7a111443e53ab5f50
Bruno Alves <balves16@hotmail.com>
2023-06-30 23:52:05 +0200

    -

--- custom.el
+++ custom.el
@@ -11,5 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(biblio-highlight-extend-face ((t (:inherit highlight :extend t))))
- '(biblio-results-header-face ((t (:foreground "orange" :weight medium :height 1.0)))))
+ )
