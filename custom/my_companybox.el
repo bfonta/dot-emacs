3d953a693efa4807fedf68b58b7f2ff308de1a5a
Bruno Alves <balves16@hotmail.com>
2023-11-28 19:19:12 +0100

    -

---
+++ custom/my_companybox.el
@@ -0,0 +1,9 @@
+;;; my_companybox --- Summary
+;;; Code:
+;;; Commentary: https://melpa.org/#/company-box
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(provide 'my_companybox)
+;;; my_companybox ends here
