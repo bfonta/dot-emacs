5da2622a51a4729cfd072f383789bfb749a8c28e
Bruno Alves <balves16@hotmail.com>
2023-12-24 16:29:32 +0100

    simplify sshfs hydra

--- custom/my_hydra.el
+++ custom/my_hydra.el
@@ -15,16 +15,8 @@
   ("p" (dired "~/Pictures/")               "pics")
   ("s" (dired "~/Pictures/Screenshots/")   "screenshots")
 
-  ("1" (dired "~/lxplus_an/")           "AN")
-  ("2" (dired "~/klub/")                "klub")
-  ("3" (dired "~/big_ntuples/")         "big ntuples")
-  ("4" (dired "~/llr_reco_hadro/")      "reco hadro")
-  ("5" (dired "~/llr_fpgas/bye_splits") "fpgas")
-  ("6" (dired "~/llr_met/")             "met")
-  ("7" (dired "~/llr_clue3d/")          "clue3d")
-  ("8" (dired "~/llr_finite/")          "finite")
-  ("9" (dired "~/lxplus_klub/")         "lx_klub")
-  ;; ("9" (dired "~/lxplus_b2g/")          "B2G")
+  ("1" (dired "~/lxplus/")  "Lxplus")
+  ("2" (dired "~/llr/")     "LLR")
   )
 
 (global-set-key (kbd "C-x d") 'hydra-dired/body)
