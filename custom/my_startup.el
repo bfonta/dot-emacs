c1c02a8bf96d9bbd79f506005580d2ffeff1200b
Bruno Alves <balves16@hotmail.com>
2024-01-06 15:39:28 +0100

    update startup font size

--- custom/my_startup.el
+++ custom/my_startup.el
@@ -11,9 +11,9 @@
   "Font size adjustment. Inspired by https://emacs.stackexchange.com/a/44930/17066"
   (let ((width-px (nth 3 (nth 1 (frame-monitor-attributes)))))
 	(when (eq width-px 1024) ;; Home
-	  (set-face-attribute 'default nil :height 110))
+	  (set-face-attribute 'default nil :height 120))
 	(when (eq width-px 2560) ;; TP, Germany
-	  (set-face-attribute 'default nil :height 110))
+	  (set-face-attribute 'default nil :height 120))
 	)
   )
 
