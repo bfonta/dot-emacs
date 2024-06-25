a138a638f5fe9d40da5df192fe6e8d6a4d3e39bb
Bruno Alves <bruno.alves@cern.ch>
2024-06-25 20:44:08 +0200

    -

--- custom/my_tex.el
+++ custom/my_tex.el
@@ -73,10 +73,10 @@
 
 ;; enables to use @EMAIL@ anywhere and replace it with the email address when exporting to latex
 ;; https://stackoverflow.com/questions/67738804/orgmode-latex-export-email-is-not-exported
-(defun nd-email-filter (contents backend info)
-  (let ((email (plist-get info :email)))
-    (replace-regexp-in-string "@EMAIL@" email contents t)))
-(add-to-list 'org-export-filter-final-output-functions (function nd-email-filter))
+;; (defun nd-email-filter (contents backend info)
+;;   (let ((email (plist-get info :email)))
+;;     (replace-regexp-in-string "@EMAIL@" email contents t)))
+;; (add-to-list 'org-export-filter-final-output-functions (function nd-email-filter))
 
 (with-eval-after-load 'ox-latex
   (add-to-list 'org-latex-classes
--- early-init.el
+++ early-init.el
@@ -1,3 +1,13 @@
+;;; Remove startup "Warning (bytecomp): reference to free variable"
+(defvar php-defs-functions-alist)
+(defvar php-ide-lsp-command-alist)
+(defvar php-project-available-root-files)
+(defvar magit-view-git-manual-method)
+(defvar lsp-auto-configure)
+(defvar lsp-completion-enable)
+(defvar php-project--validate-php-file-as-template)
+(defvar php-project--eval-bootstrap-scripts)
+
 ;;; use-package: finds all installed packages and activates them by executing their <pkg>-autoloads.el files
 ;;; reference: https://ianyepan.github.io/posts/setting-up-use-package/
 (unless (package-installed-p 'use-package)
