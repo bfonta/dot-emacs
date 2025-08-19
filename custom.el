41eba6265d3db281fee41571e6c8909394e92506
Bruno Alves <balves16@hotmail.com>
2025-08-20 00:26:59 +0200

    Updates.

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-source-names-using-follow '("/home/bfonta/cmssw/"))
+ '(helm-source-names-using-follow '("/home/bruno/cmssw/" "/home/bfonta/cmssw/"))
  '(package-selected-packages
    '(ag bash-completion benchmark-init cern-ldap company-auctex
 		company-c-headers company-fuzzy company-quickhelp copilot
