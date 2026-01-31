4366224fef09dac5ad8dbaf1bfd2bfe39e29687e
Bruno Alves <balves16@hotmail.com>
2026-01-31 09:11:52 +0100

    Updates.

--- custom.el
+++ custom.el
@@ -5,47 +5,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(bash-completion benchmark-init cern-ldap company-auctex
-					 company-c-headers company-fuzzy company-quickhelp
-					 dired-collapse dired-rainbow dired-sidebar
-					 disk-usage exec-path-from-shell expand-region flx
-					 flycheck git-link git-timemachine go-mode gptel
-					 grep-a-lot helm-bibtex helm-cscope imenu-list
-					 impatient-mode magit markdown-mode openwith
-					 org-ref org-roam pdf-tools projectile pulsar
-					 python-mode pyvenv quelpa-use-package
-					 rainbow-delimiters restart-emacs shackle slime
-					 smooth-scrolling sudo-edit vi-tilde-fringe vundo
-					 webpaste wttrin zenburn-theme)))
+ '(package-selected-packages nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-block ((t (:background "gray30" :foreground "#FFFFFD"))))
- '(org-block-begin-line ((t (:height 0.75 :weight bold :foreground "gray5" :background "orange4" :box (:style released-button) :extend t))))
- '(org-block-end-line ((t (:height 0.75 :weight bold :foreground "gray5" :background "orange4" :box (:style released-button) :extend t))))
- '(org-cite-face ((t (:weight bold :foreground "#DC8CC3"))))
- '(org-code ((t (:height 0.65 :foreground "#7F9F7F"))))
- '(org-document-info ((t (:background "#303030"))))
- '(org-document-info-keyword ((t (:background "#303030"))))
- '(org-document-title ((t (:foreground "#6C3333" :underline nil))))
- '(org-drawer ((t (:background "#303030" :foreground "#6CA0A3"))))
- '(org-ellipsis ((t (:inherit default :weight regular :foreground "#DCDCCC" :foreground "#989890"))))
- '(org-level-1 ((t (:inherit default :weight regular :foreground "#DCDCCC" :weight bold :foreground "#6C3333"))))
- '(org-level-2 ((t (:inherit default :weight regular :foreground "#DCDCCC" :weight bold :foreground "chartreuse4"))))
- '(org-level-3 ((t (:inherit default :weight regular :foreground "#DCDCCC" :weight bold :foreground "#6a5acd"))))
- '(org-level-4 ((t (:inherit default :weight regular :foreground "#DCDCCC" :foreground "#DEB887"))))
- '(org-level-5 ((t (:inherit default :weight regular :foreground "#DCDCCC" :foreground "#BA55D3"))))
- '(org-level-6 ((t (:inherit default :weight regular :foreground "#DCDCCC" :foreground "#5C4033"))))
- '(org-link ((t (:foreground "#93E0E3" :underline (:color foreground-color :style line)))))
- '(org-meta-line ((t (:foreground "#989890"))))
- '(org-property-value ((t (:background "#2B2B2B" :foreground "#FFFFEF"))))
- '(org-special-keyword ((t (:background "#303030" :foreground "#9FC59F"))))
- '(org-table ((t (:foreground "#9FC59F"))))
- '(org-tag ((t (:background "#303030" :foreground "#9FC59F"))))
- '(org-tag-group ((t (:background "#303030" :foreground "#9FC59F"))))
- '(org-verbatim ((t (:foreground "#DC8CC3"))))
- '(org-verse ((t (:foreground "#9FC59F")))))
+ )
